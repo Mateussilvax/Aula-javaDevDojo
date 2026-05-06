c4bc4fac380c4599ae06994c10da3591a18a5258
Mateus Xavier <mateus12xavier@gmail.com>
2026-05-05 22:27:48 -0300

    return part1

---
+++ src/academy/devdojo/maratonajava/Javacore/Bintroducaometodos/test/Calculadoratest3.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.Javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.Javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class Calculadoratest3 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println(calculadora.divideDoisNum(20,0));
+    }
+}
