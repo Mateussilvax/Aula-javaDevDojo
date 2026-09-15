5bb8b469eddc43c2fee499028440797fd9fd1a33
Mateus Xavier <mateus12xavier@gmail.com>
2026-09-15 12:53:06 -0300

    criação de menu bancario no exercicio

--- src/academy/devdojo/maratonajava/Exercicios/ExecJavaCore/test/ContaBancariaTest.java
+++ src/academy/devdojo/maratonajava/Exercicios/ExecJavaCore/test/ContaBancariaTest.java
@@ -10,17 +10,46 @@ public class ContaBancariaTest {
         System.out.println("digite o nome do titular: ");
         contaBancaria.setTitular((scanner.nextLine()));
 
-        //definir valor para deposito
-        System.out.print("Informe o saldo que deseja depositar: ");
-        double valorDeposito = (scanner.nextDouble());
 
-        contaBancaria.depositar(valorDeposito);
+        int opcao;
+        do {
+            System.out.println("\n--- MENU BANCÁRIO---");
+            System.out.println("1. consultar saldo");
+            System.out.println("2. depositar");
+            System.out.println("3. sacar");
+            System.out.println("0. sair");
+            System.out.println("Escolha uma opção");
+            opcao = scanner.nextInt();
 
+            switch (opcao) {
+                case 1:
+                    System.out.println("Saldo atual: R$ " + contaBancaria.getSaldo());
+                    break;
 
-        //definir qual o valor a ser sacado
-        System.out.print("Informe o saldo que deseja sacar: ");
-        double valorSaque = (scanner.nextDouble());
+                case 2:
+                    //definir valor para deposito
+                    System.out.print("Informe o saldo que deseja depositar: ");
+                    double valorDeposito = (scanner.nextDouble());
+                    contaBancaria.depositar(valorDeposito);
+                    break;
 
-        contaBancaria.sacar(valorSaque);
+                case 3:
+                    //definir qual o valor a ser sacado
+                    System.out.print("Informe o saldo que deseja sacar: ");
+                    double valorSaque = (scanner.nextDouble());
+                    contaBancaria.sacar(valorSaque);
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando o sistema!");
+                    break;
+
+                default:
+                    System.out.println("opção invalida!");
+            }
+        }while (opcao !=0);
+
+        scanner.close();
+
     }
 }
